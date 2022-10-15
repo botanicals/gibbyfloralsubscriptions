9b9336fe947031fb76d28c0ff11609dbafdaa7c8
Talon Durrant <talonmdurrant@gmail.com>
2022-10-14 21:26:35 -0600

    added fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './layouts/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { DEFAULT: '#007097' },
+        secondary: { DEFAULT: '#A31E72' },
+      },
+      fontFamily: {
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp')],
 };
