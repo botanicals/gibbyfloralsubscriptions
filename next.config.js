5828f39388c4142f9cfca5285aa90f501da58caa
Talon Durrant <talonmdurrant@gmail.com>
2022-10-16 23:43:54 -0600

    set up temporary redirects to home page

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
     });
     return cfg;
   },
+  async redirects() {
+    return [
+      {
+        source: '/:slug',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/:slug/:slug*',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
